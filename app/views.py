e00af04bbe9af1a7076e4189449af3526b5ec419
Michaela Minjeong Kim <fabmichaela@gmail.com>
2017-02-08 18:30:23 +0900

    blank line

--- app/views.py
+++ app/views.py
@@ -161,7 +161,7 @@ def removeComments(inputFileName, outputFileName):
     outf.write(inf.readline())
 
     for line in inf:
-        if  not line.lstrip().startswith("#") and not line.lstrip().startswith("SET") and not line.lstrip().startswith("administrator command") and not line.lstrip().startswith("SELECT @"):
+        if  not line.lstrip().startswith("#") and not line.lstrip().startswith("SET") and not line.lstrip().startswith("administrator command") and not line.lstrip().startswith("SELECT @") and not line.lstrip():
             outf.write(line)
 
     inf.close()
